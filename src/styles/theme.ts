259d4849d3ddb7660f20b5b87cdd85ea31ed24db
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-03-22 17:54:39 -0500

    📦 NEW: add scaffolding

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  border:{
+    radius: "4px",
+  },
+  // Colors
+  colors:{
+        
+  },
+  typography: {
+    h1:{},
+    h2:{},
+    h3:{},
+    h4:{},
+    h5:{},
+    h6:{},
+  }
+}
