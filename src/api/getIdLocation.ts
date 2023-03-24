84067924c8093405559f44826bf7f30cb939bf14
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-03-24 17:48:00 -0500

    📦 NEW:  add function for get ID location for searchHotel

--- src/api/getIdLocation.ts
+++ src/api/getIdLocation.ts
@@ -1,14 +1,12 @@
 import axios from "axios"
 
-export const searchHotels = (date_checkin?: string ,date_checkout?: string, location_id?: string) =>{
+export const getIdLocation = (place?: string) =>{
   const options = {
     method: "GET",
-    url: "https://priceline-com-provider.p.rapidapi.com/v1/hotels/search",
+    url: "https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations",
     params: {
-      date_checkout: date_checkout,//"2023-07-23",
-      sort_order: "HDR",
-      date_checkin: date_checkin, //"2023-07-22",
-      location_id:  location_id, //"3000035821",
+      name: place,
+      search_type: "HOTEL"
     },
     headers: {
       "X-RapidAPI-Key": "3039ed9becmsh30b3d62963b1ff4p1a6367jsnfe145e558f5a",
