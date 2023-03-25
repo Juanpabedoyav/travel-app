d45a356011bc174dcfe8fa06d2655781d2c56df3
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-03-24 20:17:19 -0500

    📦 NEW: add id generator function for the locations API

--- src/api/getIdLocation.ts
+++ src/api/getIdLocation.ts
@@ -9,12 +9,12 @@ export const getIdLocation = (place?: string) =>{
       search_type: "HOTEL"
     },
     headers: {
-      "X-RapidAPI-Key": "3039ed9becmsh30b3d62963b1ff4p1a6367jsnfe145e558f5a",
+      "X-RapidAPI-Key": "5ec8346cb7msh68282be38a182bap1b201bjsnb5f02a71d620",
       "X-RapidAPI-Host": "priceline-com-provider.p.rapidapi.com"
     }
   }
   try{
-    return  axios.request(options).then((response) => response.data)
+    return  axios.request(options).then((response) => response.data[0].id)
   } catch{
     throw new Error("Error")
   }
--- src/components/HotelsList/index.tsx
+++ src/components/HotelsList/index.tsx
@@ -1,9 +1,32 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { UserSearchContext } from "../../context/UserSearch/UserSearchContext"
+import { getIdLocation } from "../../api/getIdLocation"
 
 export const HotelsList = () => {
   const {checkIn, checkOut, place} = useContext(UserSearchContext)
+  const [infoId, setInfoId] = useState("")
+ 
+  const generateIdLocation =  () =>{ 
+    if(place){
+      getIdLocation(place).then( res =>{
+        setInfoId(res)
+      })  
+      console.log(infoId)
+    }
+  }
+ 
 
+  useEffect(() => {
+    generateIdLocation()
+  },[place])
+
+
+
+
+ 
+
+
+  
   return(
     <>
             hola
