ca05a8def421f73dc7ba8598d29f2468275b5759
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-03-26 14:54:36 -0500

    📦 NEW: add dashboard Component

--- src/components/DashboardAdmin/index.tsx
+++ src/components/DashboardAdmin/index.tsx
@@ -1,6 +1,96 @@
+import { CardSc, SectionHotelsSc } from "../HotelsList/styles"
+import { CardAdmindSc, DashboardAdminSC, SectionAdminSc } from "./styles"
 
 export const DashboardAdmin = () => {
   return (
-    <div>dashboard</div>
+    <DashboardAdminSC>
+      <div className="actions">    
+        <h1>DashBoard Admin</h1>
+        <button>New Hotel</button>
+      </div>
+      <SectionAdminSc>
+        <CardAdmindSc>
+          <picture>
+            <img src={"hotel.media?.url"} alt={"hotel.name"} loading="lazy" />
+          </picture>
+          <section className="card-title">
+            <h1>{"hotel.name"}</h1>
+            <p>⭐{"hotel.starRating"}</p>
+          </section>
+          <section className="card-details">
+            <p>{"hotel.brand"}</p>
+            <p>{"checkIn"} to {"checkOut"}</p>
+            <p>{"hotel.ratesSummary.minCurrencyCode"} {"hotel.ratesSummary.minPrice"} - Per night</p>
+          </section>
+          <button>Update</button>
+          <button>Delete</button>
+        </CardAdmindSc>
+        <CardAdmindSc>
+          <picture>
+            <img src={"hotel.media?.url"} alt={"hotel.name"} loading="lazy" />
+          </picture>
+          <section className="card-title">
+            <h1>{"hotel.name"}</h1>
+            <p>⭐{"hotel.starRating"}</p>
+          </section>
+          <section className="card-details">
+            <p>{"hotel.brand"}</p>
+            <p>{"checkIn"} to {"checkOut"}</p>
+            <p>{"hotel.ratesSummary.minCurrencyCode"} {"hotel.ratesSummary.minPrice"} - Per night</p>
+          </section>
+          <button>Update</button>
+          <button>Delete</button>
+        </CardAdmindSc>
+        <CardAdmindSc>
+          <picture>
+            <img src={"hotel.media?.url"} alt={"hotel.name"} loading="lazy" />
+          </picture>
+          <section className="card-title">
+            <h1>{"hotel.name"}</h1>
+            <p>⭐{"hotel.starRating"}</p>
+          </section>
+          <section className="card-details">
+            <p>{"hotel.brand"}</p>
+            <p>{"checkIn"} to {"checkOut"}</p>
+            <p>{"hotel.ratesSummary.minCurrencyCode"} {"hotel.ratesSummary.minPrice"} - Per night</p>
+          </section>
+          <button>Update</button>
+          <button>Delete</button>
+        </CardAdmindSc>
+        <CardAdmindSc>
+          <picture>
+            <img src={"hotel.media?.url"} alt={"hotel.name"} loading="lazy" />
+          </picture>
+          <section className="card-title">
+            <h1>{"hotel.name"}</h1>
+            <p>⭐{"hotel.starRating"}</p>
+          </section>
+          <section className="card-details">
+            <p>{"hotel.brand"}</p>
+            <p>{"checkIn"} to {"checkOut"}</p>
+            <p>{"hotel.ratesSummary.minCurrencyCode"} {"hotel.ratesSummary.minPrice"} - Per night</p>
+          </section>
+          <button>Update</button>
+          <button>Delete</button>
+        </CardAdmindSc>
+        <CardAdmindSc>
+          <picture>
+            <img src={"hotel.media?.url"} alt={"hotel.name"} loading="lazy" />
+          </picture>
+          <section className="card-title">
+            <h1>{"hotel.name"}</h1>
+            <p>⭐{"hotel.starRating"}</p>
+          </section>
+          <section className="card-details">
+            <p>{"hotel.brand"}</p>
+            <p>{"checkIn"} to {"checkOut"}</p>
+            <p>{"hotel.ratesSummary.minCurrencyCode"} {"hotel.ratesSummary.minPrice"} - Per night</p>
+          </section>
+          <button>Update</button>
+          <button>Delete</button>
+        </CardAdmindSc>
+      </SectionAdminSc>
+
+    </DashboardAdminSC>
   )
 }
